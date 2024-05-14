b113943380f1988fdd649d51bb2e83c0d2845344
Jordy van Essen <jordy.esse@gmail.com>
2024-05-14 10:33:43 +0200

    Add publish script to zip src files and copy to Factorio mod dir

---
+++ release/publish.ps1
@@ -0,0 +1,15 @@
+
+param (
+    [string]$publishDir,
+    [string]$modName = "train_scheduler_0.0.1"
+)
+
+if (-not (Test-Path $publishDir)) {
+    Write-Host "Directory $publishDir does not exist" -ForegroundColor Red
+    exit 1
+}
+
+Write-Host "Publishing to $publishDir" -ForegroundColor Green
+
+Compress-Archive -Path "..\src" -DestinationPath "$modName.zip" -Force
+Copy-Item -Path "$modName.zip" -Destination $publishDir
